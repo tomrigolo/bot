0d6467262667590dd57e22eeb12e63fa7cb3e1df
tomrigolo <tomjegou10@gmail.com>
2022-01-22 22:31:04 +0100

    dl

--- Selenium/Scan/dl.py
+++ Selenium/Scan/dl.py
@@ -1,10 +1,10 @@
 import requests
 import sys
 
-def dl(lien,chapter):
-    request = requests.get(lien+chapter)
+def dl(lien):
+    request = requests.get(lien)
     with open(f'E:\Scan\Sono Bisque Doll Wa Koi Wo Suru\Sono Bisque Doll Wa Koi Wo Suru .html','ab') as f:
         f.write(request.content)
 
 
-dl(str(sys.argv[1]), str(sys.argv[2]))
+dl(str(sys.argv[1]))
