9a99112b36fbdea4e66fe9a6b9a4536dcb1f7f12
tomrigolo <tomjegou10@gmail.com>
2022-01-23 15:46:46 +0100

    scroll

--- Selenium/Scan/test.py
+++ Selenium/Scan/test.py
@@ -1,9 +1,9 @@
-import requests
+
 from selenium import webdriver
-import time
 
 
 
-driver = webdriver.Chrome("E:/Python3/Python/bot/Selenium/Scan/chromedriver.exe")
+PATH = "E:/Python3/Python/bot/Selenium/Scan/chromedriver.exe"
+driver = webdriver.Chrome(PATH)
 driver.get(f'https://ww1.mangakakalot.tv/chapter/manga-zy953881/chapter-1')
-driver.execute_script("window.scrollTo(0, 1080)")
+
--- Selenium/Scan/test2.py
+++ Selenium/Scan/test2.py
@@ -1,15 +1,20 @@
-import requests
 from selenium import webdriver
 import time
 
-
-PATH_CHROME = "E:/Python3/Python/bot/Selenium/Scan/chromedriver.exe"
+PATH = "E:/Python3/Python/bot/Selenium/Scan/chromedriver.exe"
 CHEMIN_DOSSIER = "E:\Scan\Sono Bisque Doll Wa Koi Wo Suru\Sono Bisque Doll Wa Koi Wo Suru"
 LIEN_MANGA = "https://ww1.mangakakalot.tv/chapter/manga-bs978875/chapter-"
 FORMAT = "html"
-SCROLL_PAUSE_TIME = 1
+SCROLL_PAUSE_TIME = 0.1
 
-if __name__ == "__main__":    
-    driver = webdriver.Chrome(PATH_CHROME)
-    driver.get(f'https://ww1.mangakakalot.tv/chapter/manga-zy953881/chapter-1')
-    driver.execute_script("window.scrollTo(0, 1080)")
+if __name__ == "__main__":
+    driver = webdriver.Chrome(PATH)
+    driver.get(f'{LIEN_MANGA}1')
+    i = 0
+    height = driver.execute_script("return document.body.scrollHeight")
+    while i < height:
+        driver.execute_script(f"window.scrollTo(0, {i*1080})")
+        time.sleep(SCROLL_PAUSE_TIME)
+        height = driver.execute_script("return document.body.scrollHeight")
+        i += 1
+        
